9967cbdbd2ccfd23c44934c38d477768a051bf1a
Iswori <thapaeshwori@gmail.com>
2023-02-02 12:13:56 +0545

    complete

--- cal.js
+++ cal.js
@@ -9,14 +9,14 @@ function Value(a) {
         show.value=first;
     }else{
         last+=a;
-        show.value+=last;
+        show.value+=a;
     }
-    console.log(first,last,opertors)
+    console.log(first,opertors,last)
 }
 function Opt(a) {
-    opertors=a;
+    opertors+=a;
     // console.log(opertors)
-    show.value+=opertors;
+    show.value+=a;
 }
 
 function Result(a) {
@@ -48,4 +48,27 @@ function Clear() {
     last="";
     opertors="";
     show.value="0";
+}
+
+function Partial(){
+    if(opertors.length===0){
+        let lgt=first.length;
+        let data=first.slice(0,lgt-1);
+        console.log(first,data);
+        first=data;
+        show.value=data
+    }else if(opertors.length>=1&& last.length===0){
+        let lgt=opertors.length;
+        let data=opertors.slice(0,lgt-1);
+        console.log(opertors,data);
+        opertors=data;
+        show.value=first+opertors
+    }
+    else {
+        let lgt=last.length;
+        let data=last.slice(0,lgt-1);
+        console.log(last,data,opertors);
+        last=data;
+        show.value=first+opertors+last
+    }
 }
